6e8b325e18ff6728d1a5fc43a1882c7fb15d5c6d
ps9610 <ps961001@gmail.com>
2021-03-25 23:03:29 +0900

    update data fecth

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from "react";
 
 class App extends React.Component{
+  state = {
+    isLoading : true
+  }
+  componentDidMount(){
+    setTimeout(() =>{
+      this.setState({ isLoading : false });
+    }, 6000);
+  }
   render(){
-    return <div />;
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading...": "We are ready"}</div>;
   }
 }
 
